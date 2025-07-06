4d8e55adf1be14a6417a4867dfc82e9529c9f28a
sarans91 <sarans94422@gmail.com>
2025-07-06 22:44:27 +0530

    Week 3 hands on uploaded

---
+++ Week_3_spring_hands on/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.library.repository;
+public class BookRepository {
+    public void fetchBooks() {
+        System.out.println("Fetching books from repository");
+    }
+}
---
+++ Week_3_spring_hands on/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,14 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+    private BookRepository bookRepository;
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void manageBooks() {
+        System.out.println("Managing books with repository: " + bookRepository);
+    }
+    public void testDependencyInjection() {
+        bookRepository.fetchBooks();
+    }
+}
